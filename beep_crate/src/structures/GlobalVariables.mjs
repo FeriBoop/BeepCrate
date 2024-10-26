013134c1cd3b6147e72168005a5beaad2a5e43a4
RokZerdoner <rok.zerdonern@gmail.com>
2024-10-26 18:03:44 +0200

    BEEPCRATE-1 Creating Global variables file Creating a file which contains global variables like BPM or Beats
    Signed-off-by: RokZerdoner <rok.zerdonern@gmail.com>

---
+++ beep_crate/src/structures/GlobalVariables.mjs
@@ -0,0 +1,8 @@
+/** Global variables
+ * here are defined all global variables like beats_per_minute, beats and position of track play
+ * use: var <globalVarName> = require('./GlobalVariables.mjs');
+ */
+
+export var BPM = 120; // Beats per minute (how many units will be inside 1 second - 60 means one, 120 means 2, 180 means three and 30 means 0.5)
+export var BEATS = 4; // How many units inside one larger module (e.g. 2, 3, 4, ... ,10)
+export var TRACK_PLAY_POSITION = 0; // Position from where will the play at x started playing
