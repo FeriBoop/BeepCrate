97c510c59e4188a0a88ca0b5118c60e65c18b3ed
RokZerdoner <rok.zerdonern@gmail.com>
2024-10-29 14:00:12 +0100

    Adding testing play button component Creating testing components, that creates new track and plays its tones
    Signed-off-by: RokZerdoner <rok.zerdonern@gmail.com>

---
+++ beep_crate/src/ComponentsForTesting/PlayButton.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import Track from "../structures/Track.mjs";
+
+/** Test component for play functionality
+ * USE: import Play from <path-to-this-file>
+ * @returns {Element}
+ * @constructor
+ */
+function Play(){
+    const [track, setTrack] = useState(null);
+
+    //creates new track
+    React.useEffect(() => {
+        const newTrack = new Track("Track1", '#FFFFFF');
+        newTrack.tones[19].insertNewNote(0,"4n");
+        newTrack.tones[19].insertNewNote(6,"4n");
+        newTrack.tones[19].insertNewNote(10,"4n");
+        newTrack.tones[19].insertNewNote(14,"4n");
+        newTrack.tones[19].insertNewNote(18,"4n");
+        newTrack.tones[47].insertNewNote(4,"1n");
+        newTrack.tones[47].insertNewNote(0,"8n");
+        newTrack.tones[30].insertNewNote(16,"8n");
+        newTrack.tones[30].insertNewNote(19,"4n");
+        newTrack.tones[34].insertNewNote(17,"8n");
+        newTrack.tones[34].insertNewNote(19,"4n");
+        newTrack.tones[38].insertNewNote(18,"8n");
+        newTrack.tones[38].insertNewNote(19,"4n");
+        newTrack.tones[42].insertNewNote(19,"4n");
+        newTrack.tones[46].insertNewNote(19,"4n");
+        newTrack.tones[50].insertNewNote(19,"4n");
+        newTrack.tones[54].insertNewNote(19,"4n");
+        setTrack(newTrack);
+    }, []);
+
+    //play the track
+    const handlePlay = async () => {
+        if (track) {
+            await track.playFromIndex(); // Call the play method on the Track instance
+        }
+    };
+
+    return ( //creates play button
+        <button onClick={handlePlay}>Play</button>
+    );
+}
+export default Play;
